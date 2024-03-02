974b56cc4c012b41833e86175e313ce88af051d9
Slaaayer420 <129106428+Slaaayer420@users.noreply.github.com>
2024-03-02 12:47:40 -0500

    Splash screen updated

--- app/src/main/java/com/example/uottahack6/MainActivity.java
+++ app/src/main/java/com/example/uottahack6/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000; // Splash screen duration in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Make sure this is the name of your splash screen layout file
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent that will start the next Activity.
+                Intent mainIntent = new Intent(MainActivity.this, HomePageActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish(); // Close the splash screen activity
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
