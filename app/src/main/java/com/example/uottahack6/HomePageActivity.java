25c1efefc1f5ab268170c90aa9d8e14175c5650f
Tuna <117102448+jtran2003@users.noreply.github.com>
2024-03-02 14:11:46 -0500

    Starting new trip activity

--- app/src/main/java/com/example/uottahack6/HomePageActivity.java
+++ app/src/main/java/com/example/uottahack6/HomePageActivity.java
@@ -2,7 +2,9 @@ package com.example.uottahack6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void goToNewTripActivity(View v){
+        startActivity(new Intent(HomePageActivity.this, NewTripActivity.class));
     }
 }
